2f4b9dfa633e8a1632cf213f03632b2248c27222
Elina Eickstädt <elina.eickstaedt@gmail.com>
2019-03-02 10:52:01 +0100

    Created XCode Project without Storyboard, added gitignore file

--- quote/AppDelegate.swift
+++ quote/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds);
+        let homeViewController = UIViewController();
+        homeViewController.view.backgroundColor = UIColor.lightGray;
+        window!.rootViewController = homeViewController;
+        window!.makeKeyAndVisible();
         return true
     }
 

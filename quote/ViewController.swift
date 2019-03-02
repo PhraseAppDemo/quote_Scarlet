828c7278a869cf7a9d5e794251ab2797b2bc76ff
Elina Eickstädt <elina.eickstaedt@gmail.com>
2019-03-02 10:21:51 +0100

    Initial Commit

---
+++ quote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quote
+//
+//  Created by Elina Eickstädt on 02.03.19.
+//  Copyright © 2019 Elina Eickstädt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

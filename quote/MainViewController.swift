92c3e5b9cbeebe48e9cb8bef56d91ac17c9d6de7
Elina Eickstädt <elina.eickstaedt@gmail.com>
2019-06-16 18:36:19 +0200

    added validation of radom quotes

--- quote/MainViewController.swift
+++ quote/MainViewController.swift
@@ -13,8 +13,8 @@ class MainViewController: UIViewController {
     let mainView = MainView();
     
     // Liste
-    let quotes = [NSLocalizedString("quote1", comment: ""), NSLocalizedString("quote2", comment: ""), NSLocalizedString("quote3", comment: "")];
-        
+    let quotes = [NSLocalizedString("quote1", comment: ""), NSLocalizedString("quote2", comment: ""), NSLocalizedString("quote3", comment: ""), NSLocalizedString("quote4", comment: ""), NSLocalizedString("quote5", comment: ""), NSLocalizedString("quote6", comment: "")];
+    
     override func loadView() {
         self.view = self.mainView
     }
@@ -26,15 +26,18 @@ class MainViewController: UIViewController {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(loadNewQuote))
         self.mainView.logoImageView.isUserInteractionEnabled = true
         self.mainView.logoImageView.addGestureRecognizer(tapGesture)
-        
         self.mainView.quoteLabel.text = self.quotes[0]
     }
     
     @objc func loadNewQuote() {
         print("load new quote")
         let randomquote = quotes.randomElement()!
-        
+        let actualquote = "quote1"
+        while randomquote == actualquote{
+            let randomquote = quotes.randomElement()!
+        }
         self.mainView.quoteLabel.text = randomquote
+        let actualquote = randomquote
     }
 }
 
